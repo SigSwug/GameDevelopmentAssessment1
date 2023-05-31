1c8d42b9af31bdda8d2068aae8ed518a100c72dc
SigSwug <bodhijames03@gmail.com>
2023-06-01 09:32:10 +1000

    Prior to class on Thursday 01/06/2023

--- Assets/MyAssets/Scripts/Managers/AudioManager.cs
+++ Assets/MyAssets/Scripts/Managers/AudioManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
+
     public AudioSource buttonClick;
 
     public void PlayButtonAudio()
     {
         buttonClick.Play();
+    }
+
+    public void AdjustVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
     }
 }
--- Assets/Scripts/UIFunctions.cs
+++ Assets/Scripts/UIFunctions.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class UIFunctions : MonoBehaviour
 {
-    public void LoadLevel(string levelName)
+    public void SceneTransitions(string levelName)
     {
+        StartCoroutine(ChangeScene());
         SceneManager.LoadScene(levelName);
     }
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(0.3f);
+    }
+
+    /*public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }*/
+
     public void ToggleUI(GameObject UIPanel)
     {
         UIPanel.SetActive(!UIPanel.activeSelf);
