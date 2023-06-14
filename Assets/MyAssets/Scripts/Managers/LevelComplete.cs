a86a628f13dcd831f24800511755880a4ff75d02
SigSwug <bodhijames03@gmail.com>
2023-06-14 15:48:32 +1000

    Players can now leave the game properly.
    Discovered a new issue with the scoreboard though

--- Assets/MyAssets/Scripts/Managers/LevelComplete.cs
+++ Assets/MyAssets/Scripts/Managers/LevelComplete.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Photon.Pun;
 
 public class LevelComplete : MonoBehaviour
 {
     public UnityEvent levelFinished;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
--- Assets/Scripts/UIFunctions.cs
+++ Assets/Scripts/UIFunctions.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class UIFunctions : MonoBehaviour
 {
+    public void DisconnectToScene(string loadLevel)
+    {
+        StartCoroutine(Disconnection(loadLevel));
+    }
+    IEnumerator Disconnection(string levelName)
+    {
+        yield return new WaitForSeconds(0.3f);
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene(levelName);
+        yield return null;
+    }
+
     public void SceneTransitions(string loadLevel)
     {
         StartCoroutine(ChangeScene(loadLevel));
